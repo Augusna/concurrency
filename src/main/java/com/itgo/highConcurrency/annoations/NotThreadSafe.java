5fb3871ec72ac8427a5606a1ae0ce4a2d6970fba
augusna <799905637@qq.com>
2019-03-31 09:30:23 +0800

    'init'

---
+++ src/main/java/com/itgo/highConcurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,15 @@
+package com.itgo.highConcurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记线程不安全的类或者写法
+ */
+@Target(ElementType.TYPE)  //注解作用目标： 类(TYPE)上
+@Retention(RetentionPolicy.SOURCE)  //在编译时会被忽略掉
+public @interface NotThreadSafe {
+    String value() default "";
+}
